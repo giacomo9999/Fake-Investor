59ad56b61f53b98e5600d573651f73d6cfa6a114
James Gary <gmarchesi888@gmail.com>
2019-11-27 16:13:40 -0500

    App connects successfully to Mongo Atlas DB.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,35 @@
 const express = require("express");
-const path = require('path'); 
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const DIST_DIR = path.join(__dirname, "../dist"); 
+const DIST_DIR = path.join(__dirname, "../dist");
 const HTML_FILE = path.join(DIST_DIR, "index.html");
+const mongoURL =
+  "mongodb+srv://Jim:tKQbT97yUkJkmxt@cluster0-bdfjd.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURL, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Database is connected");
+  },
+  err => {
+    console.log("Cannot connect to database " + err);
+  }
+);
 
 const mockResponse = {
   foo: "bar",
   bar: "foo"
 };
 
-app.use(express.static(DIST_DIR)); 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(DIST_DIR));
 
 app.get("/api", (req, res) => {
   res.send(mockResponse);
